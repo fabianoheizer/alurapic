7d504de2d7f16f821ffd971e9f53bbebbced5b76
fabianoheizer <fabianoheizer@gmail.com>
2019-12-22 17:27:54 -0300

    parte 1 - aula 6

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,29 +1,57 @@
-import { Component, OnInit } from '@angular/core';
-import { PhotoService } from '../photo/photo.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
+
 import { Photo } from '../photo/photo';
+import { PhotoService } from '../photo/photo.service';
 
 @Component({
   selector: 'app-photo-list',
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
 
     photos: Photo[] = [];
+    filter: string = '';
+    debounce: Subject<string> = new Subject<string>();
+    hasMore: boolean = true;
+    currentPage: number = 1;
+    userName: string = '';
+
 
     // manter o construtor apenas para injeção de dependencia
-    constructor(
-            private photoService: PhotoService,
-            private activatedRoute: ActivatedRoute
-    ) { }
+    constructor(private activatedRoute: ActivatedRoute, private photoService: PhotoService) { }
 
     // qualquer lógica que queiramos executar será colocada em uma fase do ciclo de vida que todo componente Angular possui.
     ngOnInit(): void {
-        const userName = this.activatedRoute.snapshot.params.userName; // parametro da url
+        this.userName = this.activatedRoute.snapshot.params.userName;
+
+        this.photos = this.activatedRoute.snapshot.data.photos
+        //Também é possível usar this.activatedRoute.snapshot.data['photos'].
+
+        // Lettable operators no RxJS: quando emitimos um valor no evento keyup, todas as emissões serão ignoradas, sendo consideradas após 300ms
+        this.debounce
+            .pipe(debounceTime(300))
+            .subscribe(filter => this.filter = filter);
+    }
+
+    ngOnDestroy(): void {
+        // BOA PRATICA: ao sair do componente, não alocaremos mais memorias com o uso do debounce
+        this.debounce.unsubscribe();
+    }
+
+    load() {
         this.photoService
-            .listFromUser(userName)
-            .subscribe(phtosURL => this.photos = phtosURL);
+                .listFromUserPaginated(this.userName, ++this.currentPage)
+                .subscribe( photo => {
+                    this.photos = this.photos.concat(photo);
+
+                    if(!this.photos.length){
+                        this.hasMore = false;
+                    }
+                });
     }
 
 }
